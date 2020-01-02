594421f264a312f04315e46f601d042fb70f5045
asmit22 <asmitadhikari22@gmail.com>
2020-01-02 22:56:20 +0545

    rearranged

--- app/src/main/java/com/asmit/cabb/CustomerSettingsActivity.java
+++ app/src/main/java/com/asmit/cabb/CustomerSettingsActivity.java
@@ -1,15 +1,24 @@
 package com.asmit.cabb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EdgeEffect;
 import android.widget.EditText;
 
+import com.google.android.gms.maps.model.Marker;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CustomerSettingsActivity extends AppCompatActivity {
 
@@ -21,6 +30,8 @@ public class CustomerSettingsActivity extends AppCompatActivity {
     private DatabaseReference mCustomerDatabase;
 
     private String userID;
+    private String mName;
+    private String mPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +47,72 @@ public class CustomerSettingsActivity extends AppCompatActivity {
         mBack = (Button) findViewById(R.id.back);
 
 
-        mAuth =FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
-        userID=mAuth.getCurrentUser().getUid();
+        userID = mAuth.getCurrentUser().getUid();
 
         mCustomerDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child("Customers").child(userID);
+
+        getUserInfo();
+
+        mConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveUserInformation();
+            }
+        });
+
+        mBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                return;
+
+            }
+        });
+
+    }
+
+    private void getUserInfo(){
+        mCustomerDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists() && dataSnapshot.getChildrenCount()>0){
+                    Map<String,Object> map =(Map<String, Object>) dataSnapshot.getValue();
+                    if (map.get("name")!=null){
+                        mName=map.get("name").toString();
+                        mNameField.setText(mName);
+                    }
+                    if (map.get("phone")!=null){
+                        mPhone=map.get("phone").toString();
+                        mPhoneField.setText(mPhone);
+                    }
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void saveUserInformation() {
+
+        mName =mNameField.getText().toString();
+        mPhone =mPhoneField.getText().toString();
+
+        //Save user information into the database
+
+        Map userInfo= new HashMap();
+        userInfo.put("name", mName);
+        userInfo.put("phone", mPhone);
+        mCustomerDatabase.updateChildren(userInfo);
+
+        finish();
+
 
 
     }
